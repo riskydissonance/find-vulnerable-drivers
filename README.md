16fb4c629d13a1a832e104c0c3577a4929bea3c6
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2023-05-31 15:21:02 +0100

    Update README
    Add checks for if the driver is running
    
    Better printing
    
    Fix only checking one hash
    
    Print vuln service names

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 Find .sys files on disk and compare them against known vulnerable file hashes from https://www.loldrivers.io.
 
+It will then print the file location, the hash and filename according to loldrivers.io, plus if the driver is running and if so the service name.
+
+## Usage
+
+Clone and run
+
+```
+powershell -ep bypass
+. .\find-vulnerable-drivers.ps1
+```
+
+Quick usage:
+
+```
+powershell -ep bypass { iwr https://raw.githubusercontent.com/m0rv4i/find-vulnerable-drivers/master/find-vulnerable-drivers.ps1 | select -ExpandProperty content | iex }
+```
+
 ## Credits
 
 Inspired by work done by [@api0cradle / @oddvar.moe](https://gist.githubusercontent.com/api0cradle/d52832e36aaf86d443b3b9f58d20c01d/raw/94f72cf5639e006aff7a69678e84f5a868ec7e79/check_vulnerabledrivers.ps1)
--- find-vulnerable-drivers.ps1
+++ find-vulnerable-drivers.ps1
@@ -1,6 +1,10 @@
 function Get-LatestVulnerableDrivers {
-    Write-Output "Getting latest loldriver.io info..."
+    Write-Output "[*] Getting latest loldriver.io info..."
     $global:lolDrivers = (New-Object System.Net.WebClient).DownloadString("https://www.loldrivers.io/api/drivers.json") | ConvertFrom-Json
+}
+
+function Get-RunningServices {
+    $global:runningServices = gwmi win32_systemdriver| where {$_.State -eq "Running"} | select pathname
 }
 
 function Find-VulnerableDrivers{
@@ -8,22 +12,31 @@ function Find-VulnerableDrivers{
     if($global:lolDrivers -eq $null){
         Get-LatestVulnerableDrivers
     }
+    if($global:runningServices -eq $null){
+        Get-RunningServices
+    }
 
-    Write-Output "Enumerating drives..."
+    Write-Output "[*] Enumerating drives..."
     $drives =  Get-PSDrive | where {$_.Provider.ToString() -eq 'Microsoft.PowerShell.Core\FileSystem' } | select Root
 
     foreach($drive in $drives){
-        Write-output "Checking $($drive.Root) ..."
+        Write-output "[*] Checking $($drive.Root) ..."
         $drivers = Get-ChildItem -Path $drive.Root -Recurse -Include "*.sys" -ErrorAction SilentlyContinue
-        Write-output("Checking $($drivers.Count) drivers against loldrivers.io")
+        Write-output("[*] Checking $($drivers.Count) drivers against loldrivers.io")
         foreach($driver in $drivers){
-            $hash = Get-FileHash -Path $driver.FullName
-            if($global:lolDrivers.KnownVulnerableSamples.SHA256.Contains($hash.HASH.ToString().ToLower())){
-                Write-Output("$($driver.FullName) is vulnerable with a matching SHA256 hash of $($hash.HASH)")
+            $hash = (Get-FileHash -Path $driver.FullName).HASH.ToString().ToLower()
+            if($global:lolDrivers.KnownVulnerableSamples.SHA256.Contains($hash)){
+                $thisLolDriver = $global:lolDrivers | where {if($_.KnownVulnerableSamples.SHA256 -ne $null) {$_.KnownVulnerableSamples.SHA256.Contains($hash)}} | where {$_.KnownVulnerableSamples.FileName.Trim() -ne ''}
+                $thisLolDriver = ($thisLolDriver.KnownVulnerableSamples.FileName | Sort-Object | Get-Unique) -join '/'
+                Write-Output("[!] Vulnerable driver found on disk: $($driver.FullName) ($($hash) -> $($thisLolDriver))")
+                if($global:runningServices.Contains($driver.FullName)){
+                    $driverServices = gwmi win32_systemdriver| where {$_.State -eq "Running"} | where {if($_.PathName -ne $null) {$_.PathName.Contains($driver.FullName)}} | select Name
+                    $driverServices = $driverServices -join '/'
+                    Write-Output("[!] `tVulnerable driver is running! - Service(s): $driverServices")
+                }
             }
         }
     }
-
 }
 
 Find-VulnerableDrivers
